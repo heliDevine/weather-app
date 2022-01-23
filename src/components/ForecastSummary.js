ba3a5ad54f3c6720525d01346ad7df4bb8148ff0
Heli Devine <heli.devine@gmail.com>
2022-01-23 16:21:16 +0000

    forecast summaries test done

--- src/components/ForecastSummary.js
+++ src/components/ForecastSummary.js
@@ -4,9 +4,9 @@ import PropTypes from 'prop-types';
 const ForecastSummary = props => {
     const { date, temperature, description, icon } = props;
     return (
-        <div className="forecast-summary">
+        <div className="forecast-summary" data-testid="forecast-summary">
             <div className="forecast-summary__date">{date}</div>
-            {/* <div className="forecast-summary__icon">{icon}</div> */}
+
             <div className="forecast-summary__icon" data-testid="forecast-icon">
                 {icon}
             </div>
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
-xtest('renders to weather app', () => {
+test('renders to weather app', () => {
     render(<App />);
     const linkElement = screen.getByText(/Weather App/i);
     expect(linkElement).toBeInTheDocument();
