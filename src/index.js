bcc8f06efbf8dda29a11d038c63e430f820472c2
Heli Devine <heli.devine@gmail.com>
2022-01-23 14:29:06 +0000

    plumbing done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './components/App';
-import location from './data/forecast.json';
+// import { location, forecasts } from './data/forecast.json';
+
+// ReactDOM.render(
+//     <React.StrictMode>
+//         <App forecasts={forecasts} location={location.location} />
+//     </React.StrictMode>,
+//     document.getElementById('root'),
+// );
+
+import packageInfo from './data/forecast.json';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App location={location.location} />
+        <App
+            forecasts={packageInfo.forecasts}
+            location={packageInfo.location}
+        />
     </React.StrictMode>,
     document.getElementById('root'),
 );
