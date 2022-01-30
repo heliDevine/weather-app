08f7851af2f0aef39d1624e2bdc0e63a04aa626b
Heli Devine <heli.devine@gmail.com>
2022-01-30 21:34:18 +0000

    working with search bar

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,10 @@ import LocationDetails from './LocationDetails';
 import ForecastSummaries from './ForecastSummaries';
 import ForecastDetails from './ForecastDetails';
 import getForecast from '../requests/getForecast';
+import SearchForm from './SearchForm';
 
 const App = () => {
+    const [searchText, setSearchText] = useState('');
     const [forecasts, setForecasts] = useState([]);
     const [location, setLocation] = useState({ city: '', country: '' });
     const [selectedDate, setSelectedDate] = useState(0);
@@ -17,10 +19,15 @@ const App = () => {
         getForecast(setSelectedDate, setForecasts, setLocation);
     }, []);
     const handleForecastSelect = date => setSelectedDate(date);
+    const handleCitySearch = () => {
+        getForecast(setSelectedDate, setForecasts, setLocation);
+    };
     return (
         <div className="weather-app">
-            <h1>Weather app</h1>
+            {/* <h1>Weather app</h1> */}
+
             <LocationDetails city={location.city} country={location.country} />
+            <SearchForm setSearchText />
             <ForecastSummaries
                 forecasts={forecasts}
                 onForecastSelect={handleForecastSelect}
---
+++ src/components/SearchForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SearchForm = ({ searchText, setSearchText }) => {
+    const handleInputChange = event => setSearchText(event.target.value);
+    return (
+        <div className="search-form">
+            <input
+                type="text"
+                onChange={handleInputChange}
+                value={searchText}
+                id="location-search"
+                placeholder="City, Country"
+            />
+            <button type="submit" onClick={onSubmit}>
+                {' '}
+                Search{' '}
+            </button>
+        </div>
+    );
+};
+
+export default SearchForm;
+
+SearchForm.propTypes = {
+    searchText: PropTypes.string.isRequired,
+    setSearchText: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+};
